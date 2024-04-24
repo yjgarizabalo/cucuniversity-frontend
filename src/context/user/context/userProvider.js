f4d10387cda1fdcba79f52465148385a890d2057
yairg <yjgarizabalo@gmail.com>
2024-04-24 07:25:03 -0500

    [cuc-usa] add userprovider

--- src/context/user/context/userProvider.js
+++ src/context/user/context/userProvider.js
@@ -1,13 +1,119 @@
-import { useReducer, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useReducer } from 'react';
+import { useActiveLink } from 'src/routes/hooks';
+import { paths } from 'src/routes/paths';
+import { useHandleResponseMessage } from 'src/hooks/use-handler-rosponse-msg';
+import reducer from './userRedurcer';
 import { initialState } from '../initalState';
 import { UserContext } from './userContetxt';
-import reducer  from './userRedurcer'
+import { useUsersApi } from '../hooks/useUserApi';
+import { useUserDispatch  } from '../hooks/useUserDispatch';
 
 
 // eslint-disable-next-line react/prop-types
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const isUserListViewActive = useActiveLink(paths.dashboard.user.list);
+  const { handleErrorMessageNotickBar } = useHandleResponseMessage();
+  const { fetchUsers, fetchUserById, addUser, updateUser, deleteUser, multiDeleteUser } = useUsersApi();
+  const {
+    loadingAction,
+    errorAction,
+    getUsersSuccess,
+    getUserByIdSuccess,
+    addUserSuccess,
+    editUserSuccess,
+    deleteUserSuccess,
+    multiDeleteUsersSuccess,
+  } = useUserDispatch();
 
+  useEffect(() => {
+    if (!isUserListViewActive) {
+      loadingAction(dispatch);
+    }
+  }, [isUserListViewActive, loadingAction]);
+
+  const getUser = useCallback(async () => {
+    loadingAction(dispatch);
+    try {
+      const users = await fetchUsers();
+      getUsersSuccess(dispatch, users);
+    } catch (error) {
+      console.error('se a presentado un error', error);
+      errorAction(dispatch);
+      handleErrorMessageNotickBar(error);
+    }
+  }, [loadingAction, errorAction, handleErrorMessageNotickBar, fetchUsers, getUsersSuccess]);
+
+  const getUserById = useCallback(
+    async (id) => {
+      loadingAction(dispatch);
+      try {
+        const user = await fetchUserById(id);
+        getUserByIdSuccess(dispatch, user);
+      } catch (error) {
+        console.error('se a presentado un error', error);
+        errorAction(dispatch);
+        handleErrorMessageNotickBar(error);
+      }
+    },
+    [loadingAction, getUserByIdSuccess, errorAction, handleErrorMessageNotickBar, fetchUserById]
+  );
+
+  const addUserAccion = useCallback(
+    async (user) => {
+      try {
+        const newUser = await addUser(user);
+        addUserSuccess(dispatch, newUser);
+      } catch (error) {
+        console.error('error connection', error);
+        handleErrorMessageNotickBar(error);
+        throw error;
+      }
+    },
+    [addUserSuccess, handleErrorMessageNotickBar, addUser]
+  );
+
+  const editUser = useCallback(
+    async (user) => {
+      try {
+        const userUpdate = await updateUser(user);
+        editUserSuccess(dispatch, userUpdate);
+      } catch (error) {
+        console.error('error connection', error);
+        handleErrorMessageNotickBar(error, 'usuario no encontrado');
+        throw error;
+      }
+    },
+    [editUserSuccess, handleErrorMessageNotickBar, updateUser]
+  );
+
+  const deleteUserAccion = useCallback(
+    async (id) => {
+      try {
+        await deleteUser(id);
+        deleteUserSuccess(dispatch, id);
+      } catch (error) {
+        console.error('error connection', error);
+        handleErrorMessageNotickBar(error);
+        throw error;
+      }
+    },
+    [deleteUserSuccess, handleErrorMessageNotickBar, deleteUser]
+  );
+
+/*   const multiDeleteUsersAction = useCallback(
+    async (ids) => {
+      try {
+        await multiDeleteUser(ids);
+        multiDeleteUsersSuccess(dispatch, ids);
+      } catch (error) {
+        console.error('error connection', error);
+        handleErrorMessageNotickBar(error);
+      }
+    },
+    [multiDeleteUsersSuccess, handleErrorMessageNotickBar, multiDeleteUser]
+  );
+ */
 
 
   const memorizedState = useMemo(
@@ -16,16 +122,19 @@ export const UserProvider = ({ children }) => {
       userSelected: state.userSelected,
       loading: state.loading,
       error: state.error,
-      // getUser,
-      // getUserById,
-      // addUser,
-      // editUser,
-      // updateUser,
-      // deleteUser,
+      getUser,
+      getUserById,
+      addUserAccion,
+      editUser,
+      deleteUserAccion,
       // multiDeleteUser,
     }),
     [
-      // getUser,
+      getUser,
+      getUserById,
+      addUserAccion,
+      editUser,
+      deleteUserAccion,
       state.users,
       state.userSelected,
       state.loading,
